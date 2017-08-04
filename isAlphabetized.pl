77b1d23add2d65aafadd4a0675e24d0db7fbeb8e
Martin Strobel <marstr@microsoft.com>
2017-08-04 16:33:17 -0700

    Fixing isAlpha

--- isAlphabetized.pl
+++ isAlphabetized.pl
@@ -1,9 +1,9 @@
 # Looks at a stream line by line and finds duplicates or incorrect alphabetization.
 my $prev = <STDIN>;
-my $exitStatus = 0
+my $exitStatus = 0;
 while(my $current = <STDIN>) {
     if($current le $prev ){
-        print STDERR $current
+        print STDERR $current;
         $exitStatus = 1;
     }
     $prev = $current
