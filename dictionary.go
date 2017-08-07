29708a4a589fb9563e51a4dfd5bc488ee58720f6
Martin Strobel <marstr@microsoft.com>
2017-08-07 16:55:04 -0700

    Fixing 1.8 dependency

--- dictionary.go
+++ dictionary.go
@@ -1,10 +1,6 @@
 package randname
 
-import (
-	"sort"
-
-	"github.com/marstr/collection"
-)
+import "github.com/marstr/collection"
 
 type trieNode struct {
 	IsWord   bool
@@ -36,41 +32,6 @@ func (node *trieNode) Navigate(word string) *trieNode {
 		}
 	}
 	return cursor
-}
-
-func (node trieNode) Enumerate(cancel <-chan struct{}) collection.Enumerator {
-	var enumerateHelper func(trieNode, string)
-
-	results := make(chan interface{})
-
-	enumerateHelper = func(subject trieNode, prefix string) {
-		if subject.IsWord {
-			select {
-			case results <- prefix:
-			case <-cancel:
-				return
-			}
-		}
-
-		alphabetizedChildren := []rune{}
-		for letter := range subject.Children {
-			alphabetizedChildren = append(alphabetizedChildren, letter)
-		}
-		sort.Slice(alphabetizedChildren, func(i, j int) bool {
-			return alphabetizedChildren[i] < alphabetizedChildren[j]
-		})
-
-		for _, letter := range alphabetizedChildren {
-			enumerateHelper(*subject.Children[letter], prefix+string(letter))
-		}
-	}
-
-	go func() {
-		defer close(results)
-		enumerateHelper(node, "")
-	}()
-
-	return results
 }
 
 // Dictionary is a list of words
